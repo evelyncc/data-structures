f1ecb2bb17b23398e1e15a87d96f273534c8d766
Evelyn <evelynchan92@gmail.com>
2018-03-07 12:41:35 -0800

    Removed duplicate variable size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,6 +1,5 @@
 var Stack = function() {
   var someInstance = {};
-  var size = 0;
   var currentIndex = 0;
 
   // Use an object with numeric keys to store values
@@ -10,23 +9,21 @@ var Stack = function() {
   someInstance.push = function(value) {
     someInstance[currentIndex] = value;
     var pushIndex = currentIndex;
-    size++
     currentIndex++;
     return someInstance[pushIndex];
   };
 
   someInstance.pop = function() {
-    if (size > 0) {
+    if (currentIndex > 0) {
       var popIndex = currentIndex - 1;
       storage[popIndex] = someInstance[popIndex];
-      size--;
       currentIndex--;
       return storage[popIndex];
     }
   };
 
   someInstance.size = function() {
-    return size;
+    return currentIndex;
   };
 
   return someInstance;
