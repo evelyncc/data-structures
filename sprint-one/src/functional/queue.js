b7be98d511725b99415fbd85bf5fe306646fb7f0
Evelyn <evelynchan92@gmail.com>
2018-03-07 13:24:09 -0800

    Added enqueue, dequeue, and size function to Queue function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var firstIndex = 0;
+  var currentIndex = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[currentIndex] = value;
+    var pushIndex = currentIndex;
+    currentIndex++;
+    return someInstance[pushIndex];
   };
 
   someInstance.dequeue = function() {
+    if (currentIndex > 0) {
+      storage[firstIndex] = someInstance[firstIndex];
+      delete someInstance[firstIndex]
+      var popIndex = firstIndex;
+      firstIndex++;
+      return storage[popIndex];
+    }
   };
 
   someInstance.size = function() {
+    if (currentIndex - firstIndex < 0) {
+      return 0;
+    }
+    return currentIndex - firstIndex;
   };
 
   return someInstance;
