e807671606508ec69f2cbdbc6a3ac8ae44da62cc
Evelyn <evelynchan92@gmail.com>
2018-03-07 20:06:37 -0800

    Added Stack and Queue in es6 method

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.firstIndex = 0;
+    this.currentIndex = 0;
+    this.storage = {};
+  };
 
+  enqueue(value) {
+    this.storage[this.currentIndex] = value;
+    this.currentIndex++;
+    return this.storage[this.currentIndex - 1];
+  };
+
+  dequeue() {
+    if (this.currentIndex > this.firstIndex) {
+    var dequeueValue = this.storage[this.firstIndex];
+    this.firstIndex++
+    return dequeueValue;
+    }
+  };
+
+  size() {
+    if (this.currentIndex - this.firstIndex < 0) {
+      return 0;
+    } else {
+      return this.currentIndex - this.firstIndex;
+    }
+  };
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.currentIndex = 0;
+    this.storage = {};
+  };
 
+  push(value) {
+    this.storage[this.currentIndex] = value;
+    this.currentIndex++;
+    return this.storage[this.currentIndex - 1];
+  };
+
+  pop() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      var popValue = this.storage[this.currentIndex];
+      delete this.storage[this.currentIndex];
+      return popValue;
+    }
+  };
+
+  size() {
+    return this.currentIndex;
+  };
 }
