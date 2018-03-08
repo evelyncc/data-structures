e64cf43e382e90f421bb794e4c0bc3e26803080f
Evelyn <evelynchan92@gmail.com>
2018-03-07 16:08:33 -0800

    Added push, pop, and size methods to functional-shared function

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {
+    someInstance: {},
+    currentIndex: 0,
+    storage : {}
+  };
+
+  extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
+var extend = function(obj, methodsObj) {
+  for (var key in methodsObj) {
+    obj[key] = methodsObj[key];
+  }
+  return obj;
+}
 
-
+var stackMethods = {
+  push: function(value) {
+    var pushIndex = this.currentIndex;
+    this.storage[pushIndex] = value;
+    this.currentIndex++
+    return this.storage[pushIndex];
+  },
+  pop: function() {
+    if (this.currentIndex > 0) {
+    this.currentIndex--;
+    var popIndex = this.currentIndex;
+    var popValue = this.storage[popIndex];
+    delete this.storage[popIndex];
+    return popValue;
+    }
+  },
+  size: function() {
+    return this.currentIndex;
+  }
+};
