ecd2632fb889b3a22b5b3f18a25001cf959ac1cc
Evelyn <evelynchan92@gmail.com>
2018-03-07 17:57:55 -0800

    Added push, pop, and size methods to Stack in prototypal method

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstance = Object.create(stackMethods)
+  someInstance.currentIndex = 0;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.currentIndex] = value;
+    this.currentIndex++;
+    return this.storage[this.currentIndex - 1];
+  },
+  pop: function() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      var popValue = this.storage[this.currentIndex];
+      delete this.storage[this.currentIndex];
+      return popValue;
+    }
+  },
+  size: function() {
+    return this.currentIndex;
+  }
+};
